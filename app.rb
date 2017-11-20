779ff05f37a5a09b7483be0feb13445a6eb4ad6f
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-20 16:05:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/name'
+  get '/name' do
     "My name is Chris"
   end
+
+  get '/hometown' do
+    "My home town is Hoboken"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Paint-It Black'"
+  end
+
 end
